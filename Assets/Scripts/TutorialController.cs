acc8c567a1395a073a53a12f0ef9c9439f9b8d1d
hyejinn226 <jtt0226@gmail.com>
2026-04-20 00:55:40 +0900

    nextbutton

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -1,19 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
-    // 튜토리얼 Canvas 전체를 드래그해서 넣을 변수
     public GameObject tutorialCanvas;
+    public Image tutorialDisplay;   // 이미지가 교체될 UI Image 컴포넌트
+    public Sprite[] tutorialImages; // 이미지 넣기
 
-    // 버튼에 연결할 함수 (Public이어야 함)
+    private int currentIndex = 0;   // 몇 번째 장인지 기억
+
+    void Start()
+    {
+        // 시작할 때 첫 번째 이미지
+        if (tutorialImages.Length > 0)
+        {
+            tutorialDisplay.sprite = tutorialImages[0];
+        }
+    }
+
+    // [Next 버튼에 연결할 함수]
+    public void NextPage()
+    {
+        currentIndex++; // 다음 장으로 번호 증가
+
+        // 아직 보여줄 이미지가 남았다면 교체
+        if (currentIndex < tutorialImages.Length)
+        {
+            tutorialDisplay.sprite = tutorialImages[currentIndex];
+            Debug.Log("다음 페이지: " + currentIndex);
+        }
+        else
+        {
+            // 마지막 장에서 더 누르면 튜토리얼 종료
+            CloseTutorial();
+        }
+    }
+
+    // [닫기 버튼에 연결할 함수]
     public void CloseTutorial()
     {
-        // Canvas 객체를 비활성화해서 화면에서 숨김
         tutorialCanvas.SetActive(false);
-
-        // (선택) 닫기 사운드 재생이나 다른 로직 추가 가능
-        Debug.Log("Tutorial Closed!");
+        Debug.Log("튜토리얼 종료!");
     }
 }
